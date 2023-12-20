2eb4821ea52d027cdd32f77938a0474cd173c43e
Yan <zyyang513@gmail.com>
2023-12-20 11:47:12 -0600

    scrapy + splash

--- splash_tracker/splash_tracker/spiders/casetracker.py
+++ splash_tracker/splash_tracker/spiders/casetracker.py
@@ -1,3 +1,6 @@
+import json
+import time
+
 import scrapy
 from scrapy_splash import SplashRequest
 
@@ -24,7 +27,35 @@ class CasetrackerSpider(scrapy.Spider):
     '''
 
     def start_requests(self):
-        yield SplashRequest(url='https://www.casestatusext.com/cases/IOE0918743038', callback=self.parse,
+        yield SplashRequest(url='https://www.casestatusext.com/cases/IOE0918743038', callback=self.parse_onepage,
                             endpoint='execute', args={'lua_source': self.script})
+
+    def parse_onepage(self, response):
+        html_content = response.data['html']
+        json_start = html_content.rfind('cases')
+        json_end = html_content.rfind('</script></body></html>')
+        cases_html = html_content[json_start:json_end]
+        json_start = cases_html.find('[')
+        json_end = cases_html.find(']')
+        cases_array = cases_html[json_start:json_end+2]
+        start_pos = cases_array.find("IOE")
+        while start_pos != -1:
+            end_pos = cases_array.find("\\", start_pos)
+            id_value = cases_array[start_pos:end_pos]
+            start_pos = cases_array.find("IOE", end_pos)
+            time.sleep(1)
+            yield scrapy.Request(url=f'https://www.casestatusext.com/cases/{id_value}', callback=self.parse)
+
     def parse(self, response):
-        print(response.body)
+        cell = response.xpath('//td[@class="ant-descriptions-item-content"]/span/text()').extract_first()
+        status = response.xpath('//ul[contains(@class,"ant-timeline")]')
+        timelines = status.xpath('.//li')
+        for timeline in timelines:
+            time = timeline.xpath('.//div[@class="ant-timeline-item-label"]/text()').get()
+            status = timeline.xpath('.//div[@class="ant-timeline-item-content"]/text()').get()
+            print(time,status)
+            yield{
+                'case_number': cell,
+                'time': time,
+                'status': status,
+            }
